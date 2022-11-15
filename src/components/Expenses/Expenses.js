57c5f40021306ae96b1464b86d87656b36bdeea5
Erik Finnholm <erik.finnholm@gmail.com>
2022-11-15 22:00:43 +0200

    Expense Project commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+export default function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState("2020");
+
+  const getFilterYear = (yearData) => {
+    setSelectedYear(yearData);
+    console.log(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={selectedYear}
+          onGetFilterYear={getFilterYear}
+        />
+
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
